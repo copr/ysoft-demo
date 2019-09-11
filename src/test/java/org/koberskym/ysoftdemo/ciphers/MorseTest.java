865221542928c4cd702b5a499e635cc0c169630a
Your Name <martinkobersky@gmail.com>
2019-09-11 20:42:07 +0200

    add basic test cases

--- src/test/java/org/koberskym/ysoftdemo/ciphers/MorseTest.java
+++ src/test/java/org/koberskym/ysoftdemo/ciphers/MorseTest.java
@@ -1,18 +1,37 @@
 package org.koberskym.ysoftdemo.ciphers;
 
+import org.jboss.logging.Param;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MorseTest {
 
+  private Morse morse = new Morse();
+
   @ParameterizedTest
-  @ValueSource(strings = {"a", "A"})
-  void encrypt(String in) {
-    System.out.println(in);
+  @CsvSource({
+      "a, .-",
+      "e, .",
+      "Morse Code", "-- --- .-. ... . / -.-. --- -.. .",
+      "I'm so smart", ".. .----. -- / ... --- / ... -- .- .-. -"
+  })
+  void encrypt_correctResults(String in, String expected) {
+    assertEquals(expected, morse.encrypt(in));
   }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"š", "ě", "ř", "ž", "ý", "á"})
+  void encrypt_exceptionThrown_unsupportedCharacters(String in) {
+    assertThrows(IllegalArgumentException.class, () -> morse.encrypt(in));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"a"})
+  void encrypt_sameResults_differentCases(String in) {}
 
   @Test
   void decrypt() {}
